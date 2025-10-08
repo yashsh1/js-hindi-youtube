71e1786e37cda7ccb6c3b65dc35939f40e49d3a9
yashsh1 <109435086+yashsh1@users.noreply.github.com>
2025-10-08 14:07:09 +0000

    commit

--- trys/start.js
+++ trys/start.js
@@ -1,1 +1,14 @@
-console.log("Yash sharma")
+// console.log("Yash sharma");
+// console.log("Hello World");
+// window.alert(`js project alart `);
+
+document.getElementById("myH1").innerText = "Hello Yash Sharma";
+document.getElementById("myP").textContent = "This is my new project for recap of js";
+
+// the innerText property sets or returns the text content of the specified node, and all its descendants.
+// the textContent property sets or returns the text content of the specified node, and all its descendants.
+
+document.getElementById("myH1").style.color = "red";
+document.getElementById("myH1").style.backgroundColor = "yellow";
+document.getElementById("myH1").style.padding = "10px";
+document.getElementById("myH1").style.textAlign = "center";
